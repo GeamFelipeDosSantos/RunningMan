73f3f557915cefabbc389375b4bf764d8526708d
Geam Felipe dos Santos <geam@edu.univali.br>
2020-06-08 01:02:57 -0300

    Adicionar fontes

---
+++ RunningApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RunningApp.Models;
+
+namespace RunningApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
